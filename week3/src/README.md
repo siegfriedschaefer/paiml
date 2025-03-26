88e98cd84226cd8f29795c98ab3c82fda80cbdae
SiggiS <siggis@schaefer.ai>
2025-03-26 22:55:52 +0100

    CryptoTap concept in Rust

---
+++ week3/src/README.md
@@ -0,0 +1,16 @@
+
+# CryptoTap
+
+We want to build a Crypto Beer Pub Fabric which someone can use for sell virtual beer and
+use it as a social location.
+
+## Pint sizes
+
+Pint (20 Imperial fluid ounces, 568 ml)
+This is the most common and iconic size for serving draught beer in English pubs. It's the standard measure for most ales, lagers, and stouts.
+
+Half Pint (10 Imperial fluid ounces, 284 ml)
+Also very common, the half pint is often chosen by those who want a smaller drink, are trying different beers, or prefer a less potent serving.
+
+Third of a Pint (6.67 Imperial fluid ounces, ~189 ml)
+Increasingly popular, especially for trying a wider variety of beers or stronger, more flavourful brews. Often served in "taster" glasses or as part of a flight.
---
+++ week3/src/main.rs
@@ -0,0 +1,83 @@
+
+#[derive(Debug)]
+struct CryptoTap {
+    keg: Keg,
+    balance: f32,
+    investor: Option<u32>
+}
+
+#[derive(Debug)]
+struct Keg {
+    style: String,
+    volume: f32,
+    level: f32,
+    price_per_liter: f32,
+}
+
+#[derive(Debug)]
+struct Pint {
+    volume: f32,
+    price: f32,
+}
+
+impl Pint {
+
+    fn new(volume: f32, price: f32) -> Pint {
+        Pint {
+            volume: volume,
+            price: price
+        }
+    }
+}
+
+fn main() {
+    println!("Hi, my friend! Welcome in my new CryptoTap!");
+    println!("We offer just one sort of beer and we are sold out last night, but we are working on it.");
+    println!("Do you want to invest in our CryptoTap?");
+    println!("We have a special offer for you: you can invest in our CryptoTap and get a part of the profit.");
+    println!("We are working on a smart contract for this, but for now we can do it manually.");
+    println!("You just need to invest the money for one keg of beer and you will get a part of the profit.");
+    println!("The price of the keg is 5.0 EUR per liter.");
+    println!("The keg has 50 liters of beer, you get 50% of the profit.");
+    println!("The profit is the difference between the price of the sold beer and the price of the keg.");
+
+    let keg = Keg {
+        style: "IPA".to_string(),
+        volume: 50.0,
+        level: 0.0,
+        price_per_liter: 5.0,
+    };
+
+    let pint = Pint::new(0.568, 5.0);
+    let half_pint = Pint::new(0.284, 3.0);
+    let third_pint = Pint::new(0.189, 1.75);
+
+    let cryptotap = CryptoTap {
+        keg: keg,
+        balance: 0.0,
+        investor: None
+    };
+
+    println!("The price of the keg is: {}", cryptotap.keg.volume * cryptotap.keg.price_per_liter);
+    println!("The price of a Pint is: {}", pint.price);
+    println!("The price of a Half Pint is: {}", half_pint.price);        
+    println!("The price of a Third Pint is: {}", third_pint.price);
+
+    println!("The profit of a Pint is: {}", pint.price - cryptotap.keg.price_per_liter * pint.volume);
+    println!("The profit of a Half Pint is: {}", half_pint.price - cryptotap.keg.price_per_liter * half_pint.volume);
+    println!("The profit of a Third Pint is: {}", third_pint.price - cryptotap.keg.price_per_liter * third_pint.volume);
+
+    let keg_volume = cryptotap.keg.volume;
+
+    let pint_profit = (pint.price - cryptotap.keg.price_per_liter) * (keg_volume / pint.volume);
+    let half_pint_profit = (half_pint.price - cryptotap.keg.price_per_liter) * (keg_volume / half_pint.volume);
+    let third_pint_profit = (third_pint.price - cryptotap.keg.price_per_liter) * (keg_volume / third_pint.volume);
+
+    println!("The profit of a Pint is: {}", pint_profit);
+    println!("The profit of a Half Pint is: {}", half_pint_profit);
+    println!("The profit of a Third Pint is: {}", third_pint_profit);
+
+    println!("Are you interested in investing in our CryptoTap?");
+    println!("How much money do you want to invest?");
+
+}

85295305b0390aa6c335c4f9fab2582e09c8447e
SiggiS <siggis@schaefer.ai>
2025-03-26 22:54:43 +0100

    Finished week2 in day 2

--- lesson1/main.rs
+++ lesson1/main.rs
@@ -6,9 +6,33 @@ fn muffinize(s: &str) -> String {
     result
 }   
 
+fn sum(numbers: &[i32]) -> i32 {
+    let mut result = 0;
+    for n in numbers {
+        result += n;
+    }
+    result
+}
+
+fn own_integer(x: i32) {
+    let y = x + 1;
+    println!("own_integer(): x is {}", y);
+}
+
 fn main() {
+
+    panic!("I'm panicking!");
+
     println!("Hello, world!");
     println!("I love muffins, especially {}", muffinize("chocolate"));
+
+    let numbers = [1, 2, 3, 4, 5];
+    println!("The sum of the numbers is {}", sum(&numbers));
+
+    let mut x = 5;
+    own_integer(x);
+    println!("x is {}", x);
+
 }
 
 
